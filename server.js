1dcd7d6a48397ee2ce01731e5a3168e3a5ede786
Josh M Thomas <71394743+Josh236061@users.noreply.github.com>
2022-11-16 17:02:35 -0600

    Initialized 'server' file

---
+++ server.js
@@ -0,0 +1,47 @@
+
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+//Uncomment once the route files are created
+const routes = require('./controllers');
+const session = require('express-session');
+const {User,Post,Comment}=require('./models');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers'); 
+
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// Create the Handlebars.js engine object
+
+const hbs = exphbs.create({helpers});
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Inform Express.js which template engine we're using
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
